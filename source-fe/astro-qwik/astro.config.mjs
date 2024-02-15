ac537d73543dd1b5dc6def05c5d6e5600c03ecae
jixinbao <jixinbao@dcloud.io>
2024-02-15 11:55:52 +0800

    feat: 调整 qwik 集成

--- source-fe/astro-qwik/astro.config.mjs
+++ source-fe/astro-qwik/astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import qwikDev from "@qwikdev/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [qwikDev()],
+});
