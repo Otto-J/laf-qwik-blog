8c4be1cfddad98c10c110cf4b27552047080b041
jixinbao <jixinbao@dcloud.io>
2024-02-15 12:07:18 +0800

    feat: 补充 vue 相关设置

--- source-fe/astro.config.mjs
+++ source-fe/astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
 
 import qwikDev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikDev()],
+  integrations: [
+    qwikDev(),
+    vue({
+      appEntrypoint: "/src/pages/_app",
+      // 如果考虑启用 jsx
+      // https://docs.astro.build/zh-cn/guides/integrations-guide/vue/
+      template: {
+        compilerOptions: {
+          // 将任意以 ion- 开头的标签当做自定义元素
+          // isCustomElement: (tag) => tag.startsWith("ion-"),
+        },
+      },
+    }),
+  ],
 });
